071cf6c61fa07fc0aa4280668b8a078d2b076e52
Sergey Holin <holin_sergey@yahoo.com>
2022-08-28 21:40:46 -0700

    portfolio is done

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -4,67 +4,60 @@ import '../../styles/Portfolio.css';
 export default function Portfolio() {
   return (
     <div>
-      {/* <center><h3 className='header'>Portfolio</h3></center> */}
     <div className="two">
     <div className="grid">
         <div className="image2" >
             <div className="in-image-link2">
-                <h3>LED Wall</h3>
-                <h5>Node/IoT</h5>
+                <h5>Mix It Up: SQL/Handlebars</h5>
             </div>
             <div className="in-image-link2-1">
-            <h6><a href="#resume">Repo Link</a></h6>
-            <h6><a href="#resume">Live Link</a></h6>
+            <h6><a href="https://github.com/sergeyholin/Mix-It-Up" target="_blank">Repo Link</a></h6>
+            <h6><a href="https://adadlaldalkmdlka.herokuapp.com/" target="_blank">Live Link</a></h6>
             </div>
         </div>
         <div className="image3" >
             <div className="in-image-link3">
-                <h3>Calculator</h3>
-                <h5>React/JavaScrip/CSS</h5>
+                <h5>Have A Laugh:Bulma/CSS</h5>
             </div>
             <div className="in-image-link2-1">
-            <h6><a href="#resume">Repo Link</a></h6>
-            <h6><a href="#resume">Live Link</a></h6>
+            <h6><a href="https://github.com/sergeyholin/Have-A-Laugh" target="_blank">Repo Link</a></h6>
+            <h6><a href="https://sergeyholin.github.io/Have-A-Laugh/" target="_blank">Live Link</a></h6>
             </div>
         </div>
         <div className="image4" >
             <div className="in-image-link4">
-                <h3>Pastel Puzzels</h3>
-                <h5>MERN Stack</h5>
+                <h5>The Tech Blog:NODE/Express/SQL/Handlebars</h5>
             </div>
             <div className="in-image-link2-1">
-            <h6><a href="#resume">Repo Link</a></h6>
-            <h6><a href="#resume">Live Link</a></h6>
+            <h6><a href="https://github.com/sergeyholin/Tech-Blog" target="_blank">Repo Link</a></h6>
+            <h6><a href="https://techblog117.herokuapp.com/" target="_blank">Live Link</a></h6>
             </div>
         </div>
         <div className="image5" >
             <div className="in-image-link5">
-                <h3>Run Buddy</h3>
-                <h5>HTML/CSS</h5>
+                <h5>Employee Tracker:NODE/MySQL2</h5>
             </div>
             <div className="in-image-link2-1">
-            <h6><a href="#resume">Repo Link</a></h6>
-            <h6><a href="#resume">Live Link</a></h6>
+            <h6><a href="https://github.com/sergeyholin/Employee-Tracker" target="_blank">Repo Link</a></h6>
+            <h6><a href="https://drive.google.com/file/d/18MKHBlPoSSBKFVT_qJ_vUg93z-jsbEuk/view" target="_blank">Live Link</a></h6>
             </div>
         </div>
         <div className="image6" >
             <div className="in-image-link6">
-                <h3>Run Buddy</h3>
-                <h5>HTML/CSS</h5>
+                <h5>Weather Dashboard:Bootstrap/jQuery/Moment</h5>
             </div>
             <div className="in-image-link2-1">
-            <h6><a href="#resume">Repo Link</a></h6>
-            <h6><a href="#resume">Live Link</a></h6>
+            <h6><a href="https://github.com/sergeyholin/Weather-Dashboard" target="_blank">Repo Link</a></h6>
+            <h6><a href="https://sergeyholin.github.io/Weather-Dashboard/" target="_blank">Live Link</a></h6>
             </div>
         </div>
         <div className="image7" >
             <div className="in-image-link7">
-                <h3>Run Buddy</h3>
-                <h5>HTML/CSS</h5>
+                <h5>Password Generator:JS</h5>
             </div>
             <div className="in-image-link2-1">
-            <h6><a href="#resume">Repo Link</a></h6>
-            <h6><a href="#resume">Live Link</a></h6>
+            <h6><a href="https://github.com/sergeyholin/Password-Generator" target="_blank">Repo Link</a></h6>
+            <h6><a href="https://sergeyholin.github.io/Password-Generator/" target="_blank">Live Link</a></h6>
             </div>
         </div>
     </div>
