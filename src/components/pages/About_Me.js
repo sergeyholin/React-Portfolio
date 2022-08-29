8604c5970163688c3228d16dda2fb0b78364ae36
Sergey Holin <holin_sergey@yahoo.com>
2022-08-28 19:08:42 -0700

    portfolio almost done

--- src/components/pages/About_Me.js
+++ src/components/pages/About_Me.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import avatar from '../../images/avatar.jpg';
+import avatar from '../../assets/images/avatar.jpg';
 import '../../styles/About_Me.css';
 export default function About_Me() {
   return (
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,17 +1,74 @@
 import React from 'react';
+import '../../styles/Portfolio.css';
 
 export default function Portfolio() {
   return (
     <div>
-      <h1>Portfolio</h1>
-      <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
-      </p>
+      {/* <center><h3 className='header'>Portfolio</h3></center> */}
+    <div className="two">
+    <div className="grid">
+        <div className="image2" >
+            <div className="in-image-link2">
+                <h3>LED Wall</h3>
+                <h5>Node/IoT</h5>
+            </div>
+            <div className="in-image-link2-1">
+            <h6><a href="#resume">Repo Link</a></h6>
+            <h6><a href="#resume">Live Link</a></h6>
+            </div>
+        </div>
+        <div className="image3" >
+            <div className="in-image-link3">
+                <h3>Calculator</h3>
+                <h5>React/JavaScrip/CSS</h5>
+            </div>
+            <div className="in-image-link2-1">
+            <h6><a href="#resume">Repo Link</a></h6>
+            <h6><a href="#resume">Live Link</a></h6>
+            </div>
+        </div>
+        <div className="image4" >
+            <div className="in-image-link4">
+                <h3>Pastel Puzzels</h3>
+                <h5>MERN Stack</h5>
+            </div>
+            <div className="in-image-link2-1">
+            <h6><a href="#resume">Repo Link</a></h6>
+            <h6><a href="#resume">Live Link</a></h6>
+            </div>
+        </div>
+        <div className="image5" >
+            <div className="in-image-link5">
+                <h3>Run Buddy</h3>
+                <h5>HTML/CSS</h5>
+            </div>
+            <div className="in-image-link2-1">
+            <h6><a href="#resume">Repo Link</a></h6>
+            <h6><a href="#resume">Live Link</a></h6>
+            </div>
+        </div>
+        <div className="image6" >
+            <div className="in-image-link6">
+                <h3>Run Buddy</h3>
+                <h5>HTML/CSS</h5>
+            </div>
+            <div className="in-image-link2-1">
+            <h6><a href="#resume">Repo Link</a></h6>
+            <h6><a href="#resume">Live Link</a></h6>
+            </div>
+        </div>
+        <div className="image7" >
+            <div className="in-image-link7">
+                <h3>Run Buddy</h3>
+                <h5>HTML/CSS</h5>
+            </div>
+            <div className="in-image-link2-1">
+            <h6><a href="#resume">Repo Link</a></h6>
+            <h6><a href="#resume">Live Link</a></h6>
+            </div>
+        </div>
     </div>
+</div>
+</div>
   );
 }
